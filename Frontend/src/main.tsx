34b1978dbecf3b355adc83f2fe139ffca11f80a5
liazemer19 <liazemer@gmail.com>
2026-03-18 09:28:06 +0200

    server connected

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,5 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+<<<<<<< HEAD
+=======
+//import './index.css'
+>>>>>>> 52ba847 (server connected)
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
